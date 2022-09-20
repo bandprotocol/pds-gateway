1e0d3bd36be850e3185270fc7db6391b27838fdf
satawatnack <s.nack1919@gmail.com>
2022-09-20 18:12:22 +0700

    crearte internal adapter

---
+++ app/adapter/standard_crypto_price/internal_service.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from app.adapter.standard_crypto_price import StandardCryptoPrice, Input, Output
+import httpx
+import os
+
+
+class InternalService(StandardCryptoPrice):
+    # URL that allow traffic from gateway only
+    api_url: str = None
+
+    def __init__(self):
+        self.api_url = os.getenv("API_URL", None)
+
+    async def call(self, input: Input) -> Output:
+        client = httpx.AsyncClient()
+        response = await client.request(
+            "GET",
+            self.api_url,
+            params={"symbols": ",".join(input["symbols"])},
+        )
+
+        response.raise_for_status()
+        response_json = response.json()
+
+        timestamp = int(datetime.now().timestamp())
+        prices = [
+            {
+                "symbol": item["symbol"],
+                "price": float(item["price"]),
+                "timestamp": timestamp,
+            }
+            for item in response_json["data"].items()
+        ]
+
+        return Output(
+            prices=prices,
+        )
