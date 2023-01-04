78482941b7ec4e08e0a7853cb86fb809a3c20b0d
satawatnack <s.nack1919@gmail.com>
2023-01-04 16:19:52 +0700

    remove unused code

--- app/utils/types.py
+++ app/utils/types.py
@@ -6,8 +6,3 @@ class VerifyErrorType(Enum):
     FAILED_VERIFICATION = "failed_verification"
     UNSUPPORTED_DS_ID = "unsupported_ds_id"
     SERVER_ERROR = "server_error"
-
-
-class ErrorResponse(BaseModel):
-    verify_error_type: str
-    msg: str
