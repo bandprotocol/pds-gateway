83e0420a427eb3c3f28814440aa9b86f32f1d378
warittornc <69615455+warittornc@users.noreply.github.com>
2023-02-20 18:30:33 +0700

    clenup config

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     VERIFY_REQUEST_URL: HttpUrl
     ALLOWED_DATA_SOURCE_IDS: list[int]
     CACHE_SIZE: int = 1000
-    TTL_TIME: str = "10m"
+    TTL: str = "10m"
     MAX_DELAY_VERIFICATION: int = 0
 
     # ADAPTER
