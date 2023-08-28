af7efc099507892e833668592806c2c56cc01d17
petekaroon <pete_k92@hotmail.com>
2023-08-28 11:37:53 +0700

    add pds forex

---
+++ adapter/standard_forex_price/internal_service.py
@@ -0,0 +1,35 @@
+import os
+
+import httpx
+
+from adapter.standard_forex_price.base import StandardForexPrice, Input, Output
+
+
+class InternalService(StandardForexPrice):
+    # URL that allow traffic from gateway only
+    api_url: str = None
+
+    def __init__(self):
+        self.api_url = os.getenv("API_URL", None)
+
+    async def call(self, input_: Input) -> Output:
+        client = httpx.AsyncClient()
+        response = await client.request(
+            "GET",
+            self.api_url,
+            params={"symbols": ",".join(input_["symbols"])},
+        )
+
+        response.raise_for_status()
+        response_json = response.json()
+
+        prices = [
+            {
+                "symbol": item["symbol"],
+                "price": float(item["price"]),
+                "timestamp": int(item["timestamp"]),
+            }
+            for item in response_json["prices"]
+        ]
+
+        return Output(prices=prices)
